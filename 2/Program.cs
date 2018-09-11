85a855eda91bf5a578718f89ec265172393758b0
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-11 11:38:41 +0300

    wrong input

--- 2/Program.cs
+++ 2/Program.cs
@@ -6,7 +6,33 @@ namespace _2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter the X coordinate ");
+            int x = getNumber();
+            Console.WriteLine("enter the Y coordinate ");
+            int y = getNumber();
+
+            if (x > 0)
+                if (y > 0)
+                {
+                    Console.WriteLine("Your point is in first quarter ");
+                }
+                else
+                {
+                    Console.WriteLine("Your quarter is in fourth quarter");
+                }
+            else if (x < 0)
+                if (y > 0)
+                {
+                    Console.WriteLine("Your point is in second quarter");
+                }
+                else
+                {
+                    Console.WriteLine("Your point is in third quarter");
+                }
+
+
+        }
+        private static Int32 getNumber()         {             string userInput = Console.ReadLine();              try { return Convert.ToInt32(userInput); }             catch             {                 Console.WriteLine("Invalid input, please try again");                 return getNumber();             }
         }
     }
 }
